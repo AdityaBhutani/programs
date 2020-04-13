32b0b6a9e22bd1077453544db1c0708d48892ccd
Aditya <aditya@Adityas-MacBook-Air-2.local>
2020-04-13 20:04:50 +0530

    using hash to implement solution

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/two-sum/solution/
 #include<iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
@@ -18,10 +19,39 @@ vector<int> twoSum(vector<int>& nums, int target) {
   return {result_i, result_j};
 }
 
+vector<int> twoSumTwoPassHash(vector<int> &nums, int target){
+  unordered_map<int, int> ump;
+  int complement;
+  int complement_index;
+  int number_index;
+  for(int i=0; i< nums.size(); i++){
+    ump.emplace(nums[i], i);
+  }
+  unordered_map<int, int>::iterator finder;
+  for(int i=0; i< nums.size(); i++){
+    complement = target - nums[i];
+    finder = ump.find(complement);
+    if(finder != ump.end() && ump[complement] != i){
+      number_index = i;
+      complement_index = finder -> second;
+      break;
+    }
+  }
+  if (number_index <= complement_index){
+    return {number_index, complement_index};   
+  }else{
+    return {complement_index, number_index};   
+  }
+}
+
 int main(){
-  vector<int> test = {2,7,11,15};
-  vector<int> twoSumResult = twoSum(test, 9);
+  vector<int> test = {3,3};
+  vector<int> twoSumResult = twoSum(test, 6);
   cout << twoSumResult[0];
   cout << twoSumResult[1];
+  cout << "RESULT USING 2 PASS HASH:" << endl;
+  vector<int> twoSumTwoPassHashResult = twoSumTwoPassHash(test, 6);
+  cout << twoSumTwoPassHashResult[0];
+  cout << twoSumTwoPassHashResult[1];
   return 0;
 }
